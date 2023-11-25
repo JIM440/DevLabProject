e1b274b62ececc3e818a747364ebc4d2ca767d60
JIM440 <118163334+JIM440@users.noreply.github.com>
2023-11-25 12:35:47 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,22 +4,56 @@ const smallImages = document.querySelectorAll('.small-image > img')
 
 smallImages.forEach(smallImage => {
     smallImage.addEventListener('click', ()=>{
-        removeActiveClass(smallImages, 'active')
+        removeActiveClassParent(smallImages, 'active')
         smallImage.parentElement.classList.add('active')
         bigImage.src = smallImage.src
     })
+})
+// // full size image
+// const fullscreen = document.querySelector('.expand')
+// fullscreen.addEventListener('click', () => {
+    
+// })
+
+// add reviews
+const reviewContainer = document.querySelector('.add-review-container')
+
+const addReview = document.querySelector('.add-review')
+
+const closeReview = document.querySelector('.close-review')
+
+addReview.addEventListener('click', () =>{
+    reviewContainer.classList.add('active')
+})
+closeReview.addEventListener('click', () =>{
+    reviewContainer.classList.remove('active')
+})
+
+// tabs for product page
+const tabBtns = document.querySelectorAll('.tab-btn')
+const tabContents = document.querySelectorAll('.tab-content')
+
+tabBtns.forEach(tabBtn => {
+    tabBtn.addEventListener('click', () => {
+         removeActiveClass(tabBtns, 'active')
+         removeActiveClass(tabContents, 'active')
+        tabBtn.classList.add('active')
+        document.querySelector(tabBtn.dataset.tabTarget).classList.add('active')
+        })
 })
 
 
 
 
-
-
-
+// general functions
 // remove active class
-const removeActiveClass = (items, className)=>{
+const removeActiveClassParent = (items, className)=>{
     items.forEach(item => {
         item.parentElement.classList.remove(className)
-        console.log(className);
+    });
+}
+const removeActiveClass = (items, className)=>{
+    items.forEach(item => {
+        item.classList.remove(className)
     });
 }
