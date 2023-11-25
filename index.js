02279455f723a547a36c23d5c21ec439bc6e148c
JIM440 <118163334+JIM440@users.noreply.github.com>
2023-11-25 08:48:31 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const countDown = new Date('nov 4, 2023 00:00:00').getTime()
+// replace big image with small images when clicked
+const bigImage = document.querySelector('.large-image-container > img')
+const smallImages = document.querySelectorAll('.small-image > img')
+
+smallImages.forEach(smallImage => {
+    smallImage.addEventListener('click', ()=>{
+        removeActiveClass(smallImages, 'active')
+        smallImage.parentElement.classList.add('active')
+        bigImage.src = smallImage.src
+    })
+})
 
 
-const days = document.querySelector('.days')
-const hours = document.querySelector('.hours')
-const mins = document.querySelector('.minutes')
-const seconds = document.querySelector('.seconds')
 
 
-const x = setInterval(()=>{
-const now = new Date().getTime()
-const distance = countDown - now
 
-console.log(distance);
 
-days.innerText = Math.floor(distance / (1000*60*60*24))
-hours.innerText = Math.floor(distance % (1000*60*60*24) / (1000*60*60))
-mins.innerText = Math.floor(distance % (1000*60*60) / (1000*60))
-seconds.innerText = Math.floor((distance % (1000*60))/1000)
-}, 1000)
+
+// remove active class
+const removeActiveClass = (items, className)=>{
+    items.forEach(item => {
+        item.parentElement.classList.remove(className)
+        console.log(className);
+    });
+}
