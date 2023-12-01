b5fc39c9512b4d5697dc8411825e6c0646aa3603
JIM440 <118163334+JIM440@users.noreply.github.com>
2023-12-01 17:57:04 +0100

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -11,11 +11,11 @@ smallImages.each((index, smallImage) => {
     bigImages.each((idx, bigImage) => {
       if ($(bigImage).attr('src') == $(smallImage).attr('src')) {
         $(bigImage).addClass('visible');
-      }        
+      }
 
-        var translateVal = index * -100;
-        var newTranslate = 'translateX('+ translateVal + '%)'
-        $(bigImage).css('transform', newTranslate)
+      var translateVal = index * -100;
+      var newTranslate = 'translateX(' + translateVal + '%)';
+      $(bigImage).css('transform', newTranslate);
     });
   });
 });
@@ -71,108 +71,94 @@ closeReviewContainer.on('click', () => {
   textareaReview.val('');
 });
 
-
 //
 // add review element
-// 
+//
 
-const reviewForm = document.querySelector('.add-review-form')
-const submitReview = document.querySelector('.add-review-form button')
+const reviewForm = $('.add-review-form');
+const submitReview = $('.add-review-form button');
 // form on submit
-reviewForm.addEventListener('submit', (e)=>{
-// prevent form default behaviour
-e.preventDefault()
-const reviewStarsSubmit = document.querySelectorAll('form .bi-star-fill.active')
-const textareaReviewSub = document.querySelector('.add-review-form textarea');
+reviewForm.on('submit', (e) => {
+  // prevent form default behaviour
+  e.preventDefault();
+  const reviewStarsSubmit = $(
+    'form .bi-star-fill.active'
+  );
+  const textareaReviewSub = $('.add-review-form textarea');
 
-if(reviewStarsSubmit.length == 0){
-  console.log("no star rating chosen");
-}
+  if (reviewStarsSubmit.length == 0) {
+    console.log('no star rating chosen');
+  } else {
+    // user profile
+    // user name
+    const userName = $('<p>');
+    userName.text('Maja Salvador');
+    userName.addClass('name');
+    // user letter
+    const userLetter = $('<div>');
+    userLetter.text(userName.text().charAt(0));
+    userLetter.addClass('letter');
+    // add to user profile
+    const userProfile = $('<div>');
+    userProfile.addClass('user-details');
+    userProfile.append(userLetter, userName);
 
-else{
-// user profile
-// user name
-const userName = document.createElement("p")
-userName.innerText = 'Maja Salvador'
-userName.classList.add('name')
-// user letter
-const userLetter = document.createElement('div')
-userLetter.innerText = userName.innerText[0]
-userLetter.classList.add('letter')
-// add to user profile
-const userProfile = document.createElement('div')
-userProfile.classList.add('user-details')
-userProfile.append(userLetter, userName)
+    // rating text
+    const description = $('<p>');
+    description.text(textareaReviewSub.val());
+    description.addClass('rating-text');
 
+    // date rated
+    const currentDate = new Date();
+    const dateRated = $('<p>');
+    dateRated.text(currentDate.getDate() + '/' + (currentDate.getMonth() + 1) + '/' + currentDate.getFullYear());
+    dateRated.addClass('date-rated');
 
-// rating text
-  const description = document.createElement('p')
-  description.innerText = textareaReviewSub.value
-  description.classList.add('rating-text')
+    // number of stars
+    const stars = $('<div>');
+    // create actve star elements
+    for (let i = 0; i < reviewStarsSubmit.length; i++) {
+      var star = $('<i>');
+      star.addClass('bi');
+      star.addClass('bi-star-fill');
+      stars.append(star);
+    }
+    // create inactive star elements
+    for (let i = 0; i < 5 - reviewStarsSubmit.length; i++) {
+      var star = $('<i>');
+      star.addClass('bi');
+      star.addClass('bi-star-fill');
+      star.addClass('inactive');
+      stars.append(star);
+    }
+    stars.addClass('stars-review');
 
-// date rated
-const currentDate = new Date()
-  const dateRated = document.createElement('p')
-  dateRated.innerText = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`
-  dateRated.classList.add('date-rated')
-  
-  // number of stars
-  const stars = document.createElement('div')
-  // create actve star elements
-for (let i = 0; i < reviewStarsSubmit.length; i++){
-  var star = document.createElement('i')
-  star.classList.add('bi')
-  star.classList.add('bi-star-fill')
-  stars.append(star)
-}
-// create inactive star elements
-for (let i = 0; i < 5 - reviewStarsSubmit.length; i++){
-  var star = document.createElement('i')
-  star.classList.add('bi')
-  star.classList.add('bi-star-fill')
-  star.classList.add('inactive')
-  stars.append(star)
-}
-stars.classList.add('stars-review')
+    // rating
+    const rating = $('<div>');
+    rating.addClass('rating');
+    rating.append(stars, dateRated);
 
-// rating
-const rating = document.createElement('div')
-rating.classList.add('rating')
-rating.append(stars, dateRated)
+    const ratingContent = $('<div>');
+    ratingContent.addClass('rating-content');
+    const reviewBox = $('<div>');
+    reviewBox.addClass('rating-review-box');
+    // add children to rated content
 
-const ratingContent = document.createElement('div')
-ratingContent.classList.add('rating-content')
-const reviewBox = document.createElement('div')
-reviewBox.classList.add('rating-review-box')
-// add children to rated content
+    ratingContent.append(rating, description);
 
-ratingContent.append(rating, description)
+    // add children to rated box
 
-// add children to rated box
+    reviewBox.append(userProfile, ratingContent);
 
-reviewBox.append(userProfile, ratingContent)
+    const ratingReviewContainer = $('.rating-review-container')
+    ratingReviewContainer.append(reviewBox);
+    const textareaReview = $('.add-review-form textarea');
+    reviewContainer.removeClass('active');
+    removeActiveClass($('form i.bi.bi-star-fill'), 'active');
+    textareaReview.val('');
 
-const ratingReviewContainer = document.querySelector('.rating-review-container')
-ratingReviewContainer.appendChild(reviewBox)
-
-alert('review added')
-
-  const textareaReview = $('.add-review-form textarea');
-  reviewContainer.removeClass('active');
-  removeActiveClass(stars, 'active');
-  textareaReview.val('');
-}
-
-})
-
-// const ratingReviewContainer = $('.rating-review-container')
-
-
-
-
-
-
-
+  }
+});
 
 // general functions
 // remove active class
